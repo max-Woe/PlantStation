80a549b45aee1f27f9415c5de244564d525ef8fe
MaxWöhlke <maxwoehlke@web.de>
2024-11-21 17:16:09 +0100

    refcator: cleanup

--- python/PlantStation/DataCollecting/ORM/AlembicModels/Base.py
+++
@@ -1,4 +0,0 @@
-# from sqlalchemy.orm import declarative_base
-#
-# # Definiere das Base-Objekt, das für alle Modelle verwendet wird
-# Base = declarative_base()
--- python/PlantStation/DataCollecting/ORM/SQLConnection/SensorTableManager.py
+++ python/PlantStation/DataCollecting/ORM/SQLConnection/SensorTableManager.py
@@ -3,7 +3,7 @@ from sqlalchemy.testing.plugin.plugin_base import logging
 
 from DataCollecting.ORM.SQLConnection.PostgresConnection import *
 
-class MeasurementTableManager:
+class SensorTableManager:
     def __init__(self):
         self.data_df = None
 
@@ -13,20 +13,20 @@ class MeasurementTableManager:
     def get_table(self):
         with self._get_session() as db_session:
             try:
-                query = "SELECT * FROM measurement"
+                query = "SELECT * FROM sensor"
                 self.data_df = pd.read_sql(query, db_session.bind)
             except Exception as e:
-                logging.error(f"Error retrieving table measurement! Error:{e}")
+                logging.error(f"Error retrieving table sensor! Error:{e}")
                 raise
         return self.data_df
 
     def get_row(self):
         return self.data_df
 
-    def add_row(self, measurement_model):
+    def add_row(self, sensor_model):
         with self._get_session() as db_session:
             try:
-                db_session.add(measurement_model)
+                db_session.add(sensor_model)
                 db_session.commit()
 
             except Exception as e:
