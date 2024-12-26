eb03ceda86a4cf2cdc894e607bbb0ec30750b57e
MaxWöhlke <maxwoehlke@web.de>
2024-12-26 22:05:31 +0100

    - refcator:   - rename class DataLakeModels to Models   - redirected database source from local to cloud - feat:   - add possibility to add multiple rows to the database

--- python/PlantStation/DataCollecting/ORM/MeasurementTableManager.py
+++ python/PlantStation/DataCollecting/ORM/MeasurementTableManager.py
@@ -4,10 +4,22 @@ from sqlalchemy.testing.plugin.plugin_base import logging
 from DataCollecting.ORM.SQLConnection.PostgresConnection import *
 
 class MeasurementTableManager:
+    """
+    Representative of the content from the measurement table. Can be used to request
+    either the full table or a single row and add single or multiple row
+    Attributes:
+        data_df(DataFrame): content from the measurement table.
+    """
     def __init__(self):
         self.data_df = None
 
     def _get_session(self):
+        """
+        This methode initializes a new postgresql session for interactions with the
+        database.
+        :return: A new PostgresConnection session
+        :rtype: PostgresConnection
+        """
         return PostgresConnection()
 
     def get_table(self):
@@ -27,6 +39,18 @@ class MeasurementTableManager:
         with self._get_session() as db_session:
             try:
                 db_session.add(measurement_model)
+                db_session.commit()
+
+            except Exception as e:
+                logging.error(f"Error writing table measurement! Error:{e}")
+
+            finally:
+                db_session.close()
+
+    def add__multiple_rows(self, measurement_model):
+        with self._get_session() as db_session:
+            try:
+                db_session.add_all(measurement_model)
                 db_session.commit()
 
             except Exception as e:
